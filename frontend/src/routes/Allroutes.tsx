c09077345acd0d073343ce2323efb95ac23da28b
Kamadi Liyanage <kamadi.liyanage@ultimo.com>
2025-10-28 13:25:55 +0530

    Add routes connection

--- frontend/src/routes/Allroutes.tsx
+++ frontend/src/routes/Allroutes.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import StudentTable from "../components/StudentTable"
+import { ToDo } from "../components/ToDo"
 
 export const AllRoutes = () => {
     return(
         <Routes>
             <Route path="/" element={<StudentTable/>}/>
-            <Route path="/lecturers" element={<StudentTable/>}/>
+            <Route path="/lecturers" element={<ToDo/>}/>
         </Routes>
     )
 }
