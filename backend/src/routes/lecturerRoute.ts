8024a7db6bf5953785ea93fe651c30e85ba3e351
Pasindu Madusha <pasindumadusha.20@cse.mrt.ac.lk>
2025-10-29 15:35:42 +0530

    Fixed lecturer api

--- backend/src/routes/lecturerRoute.ts
+++ backend/src/routes/lecturerRoute.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { StudentController } from "../controllers/studentController";
+import { LecturerController } from "../controllers/lecturerController";
 
 const router = express.Router();
 
-router.post("/add", StudentController.addNewStudent);
-router.get("/allStudents", StudentController.getAllStudents);
+router.post("/add", LecturerController.addNewLecture);
+router.get("/allLecturers", LecturerController.getAllLecturers);
 
 export default router;
